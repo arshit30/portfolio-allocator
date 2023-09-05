d09e8dbae7408bd4f819ddc0bbeadb2d6de6d4bc
arshit30 <arshitkumar26@gmail.com>
2023-09-05 10:19:32 +0530

    first working page

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask,render_template, request, redirect
+from flask import Flask,render_template, request, redirect,url_for
 
 app= Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
-@app.route('/User', methods=['GET','POST'])
-def homepage():
-    
+#@app.route('/User', methods=['GET','POST'])
+#def homepage():
+
 
 
 app.run(host='0.0.0.0',port=5000)
